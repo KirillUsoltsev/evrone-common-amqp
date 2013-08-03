5946bc9e3180ad00f62e152f41a96bb6e6e375f5
Dmitry Galinsky <dima.exe@gmail.com>
2013-08-03 22:06:00 +0400

    fixed logger in ack

--- lib/evrone/common/amqp/consumer/ack.rb
+++ lib/evrone/common/amqp/consumer/ack.rb
@@ -5,12 +5,12 @@ module Evrone
 
         def ack!(multiple = false)
           self.class.session.channel.ack delivery_info.delivery_tag, multiple
-          logger.info "commit ##{delivery_info.delivery_tag}"
+          session.info "commit ##{delivery_info.delivery_tag}"
         end
 
         def nack!(multiple = false, requeue = false)
           self.class.session.channel.ack delivery_info.delivery_tag, multiple, requeue
-          logger.info "reject ##{delivery_info.delivery_tag}"
+          session.info "reject ##{delivery_info.delivery_tag}"
         end
 
       end
